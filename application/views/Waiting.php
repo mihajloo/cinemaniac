8f71c6a5749b949e8b3559dfb5bd46616316b9bc
mihajloo <42943109+mihajloo@users.noreply.github.com>
2019-05-26 20:59:13 +0200

    Update Waiting.php

--- application/views/Waiting.php
+++ application/views/Waiting.php
@@ -20,5 +20,26 @@
   <span class="sr-only">Loading...</span>
     </div>
         <div><?php if(isset($brIgraca)) echo $brIgraca; else echo 0; ?></div>   
+        <script>  
+setInterval(function f() {
+if (window.XMLHttpRequest)
+{// code for IE7+, Firefox, Chrome, Opera, Safari
+ xmlhttp=new XMLHttpRequest();
+}
+else
+{// code for IE6, IE5
+ xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.onreadystatechange=function()
+{
+if (xmlhttp.readyState==4 && xmlhttp.status==200)
+ {
+ document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+ }
+}
+xmlhttp.open("GET","<?php $stranica = site_url('VipC/play');  echo $stranica;?>",true);
+xmlhttp.send();
+},1000);
+    </script>
     </body>
 </html>    
